0cf6093d4ceab9c13d6e66655cb7782eaf705bc9
Muhammad Ibrahim <ibrahimiftikharr@hotmail.com>
2025-07-23 18:11:34 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,39 @@ provider "oci" {
   region           = var.region
 }
 
+# Variables
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+variable "compartment_ocid" {}
+
+variable "availability_domains" {
+  type    = list(string)
+  default = ["AD-1", "AD-2", "AD-3"]
+}
+
+# Get the latest Ubuntu ARM image
+data "oci_core_images" "ubuntu_arm" {
+  compartment_id = var.compartment_ocid
+
+  operating_system         = "Canonical Ubuntu"
+  operating_system_version = "22.04"
+  shape                    = "VM.Standard.A1.Flex"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
+
+# Get available ADs (optional)
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+
+# Launch instance
 resource "oci_core_instance" "arm_instance" {
   count               = 1
-  availability_domain = element(var.availability_domains, 0) # Initially AD-1
+  availability_domain = element(var.availability_domains, 0) # Start with AD-1
   compartment_id      = var.compartment_ocid
 
   shape = "VM.Standard.A1.Flex"
@@ -28,18 +58,4 @@ resource "oci_core_instance" "arm_instance" {
   metadata = {
     ssh_authorized_keys = file("~/.ssh/id_rsa.pub")
   }
-}
-
-data "oci_identity_availability_domains" "ads" {
-  compartment_id = var.tenancy_ocid
-}
-
-data "oci_core_images" "ubuntu_arm" {
-  compartment_id = var.compartment_ocid
-
-  operating_system         = "Canonical Ubuntu"
-  operating_system_version = "22.04"
-  shape                    = "VM.Standard.A1.Flex"
-  sort_by                  = "TIMECREATED"
-  sort_order               = "DESC"
 }
